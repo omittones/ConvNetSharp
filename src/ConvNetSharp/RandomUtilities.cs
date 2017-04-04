4677c1c12c305e84195f641f63822f6ba71ed7f5
Augustin Juricic <ajuricic@neogov.net>
2017-04-04 19:37:21 +0200

    Fix Gaussian distribution when multithreading.

--- src/ConvNetSharp/RandomUtilities.cs
+++ src/ConvNetSharp/RandomUtilities.cs
@@ -5,13 +5,11 @@ namespace ConvNetSharp
     public static class RandomUtilities
     {
         private static readonly Random Random = new Random(Seed);
+
         private static double val;
         private static bool returnVal;
 
-        public static int Seed
-        {
-            get { return (int) DateTime.Now.Ticks; }
-        }
+        public static int Seed => (int) DateTime.Now.Ticks;
 
         public static double GaussianRandom()
         {
@@ -21,17 +19,21 @@ namespace ConvNetSharp
                 return val;
             }
 
-            var u = 2*Random.NextDouble() - 1;
-            var v = 2*Random.NextDouble() - 1;
-            var r = u*u + v*v;
+            double r = 0, u = 0, v = 0;
 
-            if (r == 0 || r > 1)
+            //System.Random is not threadsafe
+            lock (Random)
             {
-                return GaussianRandom();
+                while (r < double.Epsilon || r > 1)
+                {
+                    u = 2*Random.NextDouble() - 1;
+                    v = 2*Random.NextDouble() - 1;
+                    r = u*u + v*v;
+                }
             }
 
             var c = Math.Sqrt(-2*Math.Log(r)/r);
-            val = v*c; // cache this
+            val = v*c; //cache this
             returnVal = true;
 
             return u*c;
