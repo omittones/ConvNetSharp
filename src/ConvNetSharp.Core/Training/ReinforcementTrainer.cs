fdc7841a18363955faa1e6caeed0c2784fa7dfc2
Augustin Juricic <ajuricic@neogov.net>
2017-11-05 13:56:18 +0100

    Convet to double while work in progress.

--- src/ConvNetSharp.Core/Training/ReinforcementTrainer.cs
+++ src/ConvNetSharp.Core/Training/ReinforcementTrainer.cs
@@ -6,30 +6,31 @@ using System;
 
 namespace ConvNetSharp.Core.Training
 {
-    public class ReinforcementTrainer<T> : SgdTrainer<T>
-        where T : struct, IEquatable<T>, IFormattable
+    public class ReinforcementTrainer : SgdTrainer<double>
     {
-        public T Baseline { get; private set; }
-        public T RewardDiscountGamma { get; set; }
-        public T EstimatedRewards => Ops<T>.Negate(this.Loss);
+        public bool ApplyBaselineAndNormalizeReturns { get; set; }
+        public double Baseline { get; private set; }
+        public double RewardDiscountGamma { get; set; }
+        public double EstimatedRewards => Ops<double>.Negate(this.Loss);
 
-        private readonly IReinforcementLayer<T> finalLayer;
+        private readonly IReinforcementLayer<double> finalLayer;
         private readonly Random rnd;
 
         public ReinforcementTrainer(
-            Net<T> net,
+            Net<double> net,
             Random rnd) : base(net)
         {
             this.finalLayer = net.Layers
-                .OfType<IReinforcementLayer<T>>()
+                .OfType<IReinforcementLayer<double>>()
                 .FirstOrDefault();
 
             this.rnd = rnd;
-            this.Baseline = Ops<T>.Zero;
-            this.RewardDiscountGamma = Ops<T>.Zero;
+            this.Baseline = Ops<double>.Zero;
+            this.RewardDiscountGamma = Ops<double>.Zero;
+            this.ApplyBaselineAndNormalizeReturns = true;
         }
 
-        public int[] Act(Volume<T> inputs)
+        public int[] Act(Volume<double> inputs)
         {
             var output = this.Forward(inputs);
 
@@ -38,42 +39,52 @@ namespace ConvNetSharp.Core.Training
             Debug.Assert(output.BatchSize == this.BatchSize);
 
             var classCount = output.Depth;
-            var values = new T[classCount];
+            var values = new double[classCount];
             var prediction = new int[this.BatchSize];
             for (var n = 0; n < this.BatchSize; n++)
             {
 
                 int max = 0;
                 for (var j = 0; j < classCount; j++)
-                    if (Ops<T>.GreaterThan(output.Get(0, 0, j, n), output.Get(0, 0, max, n)))
+                    if (Ops<double>.GreaterThan(output.Get(0, 0, j, n), output.Get(0, 0, max, n)))
                         max = j;
                 prediction[n] = max;
             }
 
             return prediction;
         }
-        
-        public override void Train(Volume<T> x, Volume<T> y)
+
+        public override void Train(Volume<double> x, Volume<double> y)
         {
             throw new NotSupportedException("Use Reinforce method instead!");
         }
 
-        public void Reinforce(Volume<T> pathInputs, int[][] pathActions, T[] pathReturns)
+        public void Reinforce(Volume<double> pathInputs, int[][] pathActions, double[] pathReturns)
         {
             this.Forward(pathInputs);
 
+            if (ApplyBaselineAndNormalizeReturns)
+            {
+                //normalize by standard deviation
+                var stdDev = pathReturns
+                    .Select(a => { var d = a - Baseline; d = d * d; return d; })
+                    .Average();
+
+                pathReturns = pathReturns
+                    .Select(r => r / stdDev)
+                    .ToArray();
+            }
+
             this.finalLayer.SetReturns(pathActions, pathReturns, this.Baseline, this.RewardDiscountGamma);
 
-            //refit baseline to minimize Sum[(R - b)^2]
-            Baseline = Ops<T>.Zero;
-            for (var i = 0; i < pathReturns.Length; i++)
-                Baseline = Ops<T>.Add(pathReturns[i], Baseline);
-            Baseline = Ops<T>.Divide(Baseline, Ops<T>.Cast(pathReturns.Length));
- 
-            //var mean = averages.Average();
-            //averages = averages.Select(a => a - mean).ToArray();
-            //var stdev = averages.Select(a => a * a).Average();
-            //averages = averages.Select(a => a / stdev).ToArray();
+            if (ApplyBaselineAndNormalizeReturns)
+            {
+                //refit baseline to minimize Sum[(R - b)^2]
+                Baseline = Ops<double>.Zero;
+                for (var i = 0; i < pathReturns.Length; i++)
+                    Baseline = Ops<double>.Add(pathReturns[i], Baseline);
+                Baseline = Ops<double>.Divide(Baseline, Ops<double>.Cast(pathReturns.Length));
+            }
 
             this.Backward(pathInputs);
 
